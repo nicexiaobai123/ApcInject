2aec315538d4455e7db590de4ea4cea67382a7bd
nioce <nioce@163.com>
2024-03-01 11:41:21 +0800

    加入进程回调测试

--- ApcInject/Main.c
+++ ApcInject/Main.c
@@ -2,11 +2,29 @@
 #include "utils.h"
 #include "ApcInject.h"
 
+void PcreateProcessNotifyRoutineEx(
+ PEPROCESS Process,
+ HANDLE ProcessId,
+ PPS_CREATE_NOTIFY_INFO CreateInfo
+)
+{
+	if (CreateInfo)
+	{
+		DbgPrint("有进程创建了\r\n");
+	}
+	else
+	{
+		DbgPrint("有进程销毁了\r\n");
+	}
+}
+
 VOID Unload(PDRIVER_OBJECT pDriver)
 {
 	KdPrint(("[info]:Unload~\r\n"));
 
 	UninitApcStartupInject();
+	
+	PsSetCreateProcessNotifyRoutineEx(PcreateProcessNotifyRoutineEx, TRUE);
 }
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT pDriver, PUNICODE_STRING pRegPath)
@@ -15,6 +33,12 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT pDriver, PUNICODE_STRING pRegPath)
 
 	status = InitApcStartupInject();
 
+	// 连接器命令行已设置 /INTEGRITYCHECK
+	status = PsSetCreateProcessNotifyRoutineEx(PcreateProcessNotifyRoutineEx, FALSE);
+
 	pDriver->DriverUnload = Unload;
-	return status;
+
+	// 驱动一定返回成功
+	// 模块回调成功驱动加载失败则会出现驱动模块卸载了,但是回调没卸载
+	return STATUS_SUCCESS;
 }
